f8c70036bb635fa380bab5136b7cf7c2fd972042
Rebby <rebbystuff@gmail.com>
2025-07-10 19:46:17 -0400

    Updated to 1.21.1!

--- src/client/java/dev/rebby/yumecraft/YumeCraftClient.java
+++ src/client/java/dev/rebby/yumecraft/YumeCraftClient.java
@@ -2,13 +2,19 @@ package dev.rebby.yumecraft;
 
 import dev.rebby.yumecraft.particles.ClientSideParticles;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.DimensionRenderingRegistry;
 
 public class YumeCraftClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 		//SoundsHandler.register();
+
 		ClientSideParticles.register();
+
+	}
+
+	private static void registerClientListeners(){
 
 	}
 }
--- src/main/java/dev/rebby/yumecraft/util/ModDimensions.java
+++ src/main/java/dev/rebby/yumecraft/util/ModDimensions.java
@@ -4,4 +4,5 @@ public class ModDimensions {
 
     public static final String POINT_NEMO = "point_nemo";
     public static final String VERDANT_TEMPLE = "verdant_temple";
+    public static final String INFINITE_MALL = "infinite_mall";
 }
